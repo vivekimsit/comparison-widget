462e0141230d85c2fbe1fed6f1cbc5ee4ab08bfb
Cassio Greco <cassiogreco@users.noreply.github.com>
2016-10-25 17:56:26 +0100

    Revert "Ux fixes"

--- src/comparison/Provider.js
+++ src/comparison/Provider.js
@@ -25,6 +25,7 @@ export default class Provider extends Component {
     const feeBarWidth = (this.props.fees / (this.props.hiddenFees + this.props.fees) * 100);
     const title = this.popoverTitle();
     const fetched = <span>Fetched on: <Datetime date={this.props.collectedAt}/></span>
+
     return (
       <tr>
         <td className="provider-name">
@@ -34,29 +35,27 @@ export default class Provider extends Component {
         <td className="hidden-xs hidden-sm">
           <div className="m-t-3">
             <Popover trigger="hover" title={title} content={fetched}>
-              <div className="progress progress-lg help-cursor" style={{position: 'relative', width: barWidth + '%', verticalAlign: 'middle', minWidth: '35px'}}>
+              <div className="progress progress-lg" style={{position: 'relative', width: barWidth + '%', verticalAlign: 'middle', minWidth: '35px'}}>
+                {feeBarWidth < 10 ? <span className="p-r-1 hidden-text">hidden</span> : null}
                 {parseFloat(this.props.fees) > 0 ?
                   <div className="progress-bar" style={{width: feeBarWidth + '%'}}>
+                    <span className="m-l-1 pull-left">upfront</span>
                   </div> : null}
               </div>
-
-              <Amount className="h2 font-weight-normal text-no-wrap m-l-2 help-cursor" style={{verticalAlign: 'middle', margin: 0}} currency={this.props.source} value={this.props.fees + this.props.hiddenFees}/>
             </Popover>
-          </div>
-          <div>
-            <small className="text-primary">Upfront</small>
-            {this.props.hiddenFees > 0 ? <small className="text-danger m-l-2">Hidden</small> : null}
+
+            <Amount className="h2 font-weight-normal text-no-wrap m-l-2" style={{verticalAlign: 'middle', margin: 0}} currency={this.props.source} value={this.props.fees + this.props.hiddenFees}/>
           </div>
         </td>
 
         <td className="hidden-xs hidden-md hidden-lg hidden-xl text-xs-right">
           <Popover trigger="click" title={title} content={fetched}>
-            <div className="progress help-cursor" style={{width: barWidth + '%'}}>
+            <div className="progress" style={{width: barWidth + '%'}}>
               <div className="progress-bar" style={{width: feeBarWidth + '%'}}></div>
             </div>
 
           <br/>
-          <Amount className="m-b-0 h2 font-weight-normal no-text-wrap help-cursor" style={{display: 'inline-block'}} currency={this.props.source} value={this.props.fees + this.props.hiddenFees}/>
+          <Amount className="m-b-0 h2 font-weight-normal no-text-wrap" style={{display: 'inline-block'}} currency={this.props.source} value={this.props.fees + this.props.hiddenFees}/>
 
           <br/>
           <div style={{display: 'inline-block'}}>
